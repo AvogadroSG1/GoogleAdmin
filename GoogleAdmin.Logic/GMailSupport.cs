40669f7b2340999847567a41e29a10ee810ae2cb
Peter O'Connor <avogadrosg1@Gmail.com>
2021-08-16 19:08:20 -0400

    Added Work for the SendAs Setting Feature

--- GoogleAdmin.Logic/GMailSupport.cs
+++ GoogleAdmin.Logic/GMailSupport.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,11 +10,13 @@ using Google.Apis.Gmail.v1;
 using Google.Apis.Gmail.v1.Data;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
+using GoogleAdmin.Models;
 
 namespace GoogleAdmin.Logic
 {
     public class GmailSupport
     {
+        private string StandardSignature = "{0}<br><img src=\"https://lh4.googleusercontent.com/rbPPIKLbpy4Lyh6mHaTWuwoFwxaNMA-MAO20zkfP8DjRBfKrYMyNM-37MaKId1_hRGOEu82ITMXWOlqp4-K5ZV0VadYrmXOCCajohB2aFR-32kJGcmg-J1ukKfbZh5qThNl4BlKj\" style=\"margin-top:0px\" class=\"CToWUd\" width=\"217.97260273972603\" height=\"87\"><br>After Hours Client Call Center<br>(503) 207-5300 - For use between 8 PM - 8 AM Daily<br><br>215-302-7977 • PA  / 702 N. 3rd Street #40, Philadelphia, PA 19123 <br>410-656-2221 • MD / 8480 Baltimore National Pike #181 Ellicott City, MD 21043<br>202-280-1772 • DC / 100 M St SE #600, Washington, DC 20003<br>757-250-5729 • VA & NC / 780 Lynnhaven Parkway / Suite 400 Virginia Beach, VA 23452<br>305-421-7266 • FL / 2598 E Sunrise Blvd Suite 2104 Fort Lauderdale, FL 33304<br>";
         private string ApplicationName = "Gmail API .NET Quickstart";
         private string[] Scopes = {GmailService.Scope.GmailSettingsBasic, GmailService.Scope.GmailCompose, GmailService.Scope.MailGoogleCom };
         private readonly GmailService gmailService;
@@ -79,6 +83,20 @@ namespace GoogleAdmin.Logic
 
             Message test = await response.ExecuteAsync();
             Console.WriteLine(test.ToString());
+        }
+
+        public async Task SetSignature(List<GoogleUser> users)
+        {
+
+            foreach(GoogleUser user in users)
+            {
+                var signatureQuery = gmailService.Users.Settings.SendAs.List(user.PrimaryEmail);
+                signatureQuery.Fields = "isPrimary,sendAsEmail";
+
+                var signatureResponse = await signatureQuery.ExecuteAsync();
+                SendAs currentSendAs = signatureResponse.SendAs.SingleOrDefault(sa => sa.IsPrimary ?? false);
+
+            }
         }
     }
 
